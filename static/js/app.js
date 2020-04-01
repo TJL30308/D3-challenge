8c45b6a458637c5db4e7dea8a0112bfe25e5bddb
Topias Lemetyinen <topias.lemetyinen@gmail.com>
2020-04-01 10:13:20 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -6,18 +6,22 @@ function makeResponsive() {
     var margin = {
         top: 20,
         right: 40,
-        bottom: 80,
+        bottom: 60,
         left: 100
-        };
+    };
 
     var width = svgWidth - margin.left - margin.right;
     var height = svgHeight - margin.top - margin.bottom;
 
-    var svg = d3.select("scatter")
-                .append("svg")
-                .attr(width, svgWidth)
-                .attr(height, svgHeight);
-        
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+    var svg = d3.select("#scatter")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    var chartGroup = svg.append("g")
+                        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
     // read in CSV and console log check
     d3.csv("static/data/data.csv").then(data => console.log(data));
 
